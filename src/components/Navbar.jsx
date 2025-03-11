e0f87d978c1a4edb77e1730a313b8bebb9487bc1
Constance Lu <constance.dev4code@gmail.com>
2025-03-11 19:36:32 +0000

    fix legacy dependacies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className= {`${styles.paddingX} 
-      w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className= {`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between
       items-center max-w-7xl mx-auto">
@@ -24,8 +23,12 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer"> Constance <span className="sm:block hidden">| JavaScript Mastry</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer"> 
+            Constance &nbsp 
+            <span className="sm:block hidden">| JavaScript Mastry</span>
+          </p>
         </Link>
+        <p className="text-red-500">asdas</p>
 
       </div>
 
